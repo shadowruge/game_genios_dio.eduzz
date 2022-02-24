7a55a4382b7c23f3b593a4daca61eef93c109946
shadowruge <zambelestar@gmail.com>
2022-02-23 22:48:47 -0300

    meu game

---
+++ main.js
@@ -0,0 +1,19 @@
+let order = [];
+let click= [];
+let scoe= 0;
+
+const blue = document.querySelector('blue');
+const blue = document.querySelector('red');
+const blue = document.querySelector('green');
+const blue = document.querySelector('yellow');
+
+let shoflow=()=>{
+    let clororder = Math.floor(Math.random()*4);
+    order[order.length] = clororder;
+    clickedorder =[];
+
+    for(let i in order){
+        
+    }
+
+}
